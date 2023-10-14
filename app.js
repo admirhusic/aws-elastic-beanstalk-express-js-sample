c4af2d77f937cde175dd60e71ce23a68c29198d2
Admir Husic <admirhusic91@gmail.com>
2023-10-14 21:54:34 +0200

    added demo endpoint

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const port = 8080
 
 app.get("/", (req, res) => res.send("We love AWS!"))
 
+app.get("/demo", (req, res) => res.send("This is a demo endpoint"))
+
 app.listen(port)
 console.log(`App running on http://localhost:${port}`)
